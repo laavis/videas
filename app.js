380417bc79daf1539a8ef542da7379e137dbbda2
laavis <sara.suviranta@gmail.com>
2018-07-05 11:42:41 +0300

    video ideas application w/ express and node.js

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// initialize application
+const app = express();
+
+// load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// passport config
+require('./config/passport')(passport);
+
+// map global promise- get rid of warning
+mongoose.Promise = global.Promise;
+// connect to mongoose
+mongoose.connect('mongodb://localhost/videas-dev')
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+
+// handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// method override middleware
+app.use(methodOverride('_method'));
+
+// express-session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// passport middleware. It's important to put these under the express-session middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// flash middleware
+app.use(flash());
+
+// global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+// index route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+// about route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+// use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+// set port
+const port = 5000;
+
+// listen on certain port
+app.listen(port, () => {
+  console.log(`Server started on port ${port}...`);
+});
---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const localStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// load user model
+const User = mongoose.model('users');
+
+module.exports = function (passport) {
+  passport.use(new localStrategy({ usernameField: 'email' }, (email, password, done) => {
+    // match user
+    User.findOne({
+      email: email,
+    }).then(user => {
+      if(!user) {
+        return done(null, false, { message: 'No user found' });
+      }
+
+      // match password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      })
+    })
+  }));
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+}
